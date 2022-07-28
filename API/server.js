630797717674dc78b4cc0735eb2211c17b219fbd
Pasku99 <pasku99albatera@gmail.com>
2022-07-28 12:38:18 +0200

    fix: unsafe inline

--- API/server.js
+++ API/server.js
@@ -10,7 +10,43 @@ const app = express()
 
 dbConnection();
 
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: {
+        useDefaults: false,
+        "block-all-mixed-content": true,
+        "upgrade-insecure-requests": true,
+        directives: {
+            "default-src": [
+                "'self'"
+            ],
+            "base-uri": "'self'",
+            "font-src": [
+                "'self'",
+                "https:",
+                "data:"
+            ],
+            "frame-ancestors": [
+                "'self'"
+            ],
+            "img-src": [
+                "'self'",
+                "data:"
+            ],
+            "object-src": [
+                "'none'"
+            ],
+            "script-src": [
+                "'self'",
+                "https://cdnjs.cloudflare.com"
+            ],
+            "script-src-attr": "'none'",
+            "style-src": [
+                "'self'",
+                "https://cdnjs.cloudflare.com"
+            ],
+      },
+    }
+}));
 app.use(cors());
 app.use(express.json());
 
@@ -19,10 +55,10 @@ app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/keys', require('./routes/keys'));
 app.use('/api/v1/workgroups', require('./routes/workgroups'));
 
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'public', 'index.html');
-    res.sendFile(index);
-});
+app.use(express.static(path.join(__dirname, "public")));
+    app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public",'index.html'));
+})
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ', process.env.PORT)
